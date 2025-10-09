4cfd97a2c84dabdeecfe4e21e483d7582a294349
maccate04 <catemaclaughlin@gmail.com>
2025-10-09 01:03:45 -0400

    Method implementation

--- src/DanceBuilder.java
+++ src/DanceBuilder.java
@@ -1,4 +1,7 @@
 import java.lang.Math;
+import java.util.Random;
+import components.sequence.Sequence;
+import components.sequence.Sequence1L;
 
 /*
  *
@@ -26,9 +29,22 @@ public class DanceBuilder {
 
 /*
  * @return type of dance as String from list of dances
+ *
+ * @param types holds different styles of dance
  */
- private static String generateType(){
+ private static String generateType(String[] types){
+    assert types != null;
 
+    //if types has length 0, return null
+    if (types.length == 0){
+        return null;
+    }
+
+    //make a random variable and use nextInt to randomly choose a space
+    Random rand = new Random();
+    int randInd = rand.nextInt(types.length);
+
+    return types[randInd];
  }
 
 
@@ -39,14 +55,30 @@ public class DanceBuilder {
  */
  private final int numBars(){
 
+    Random rand = new Random();
+    int randInd = rand.nextInt();
+
+    //returns the random int that must be even
+    return randInd & ~1;
  }
 
 
  /*
- * @return type of dance as String from list of dances
+ * @return mood of dance as String from list of moods
  */
- private static String danceMood(){
+ private static String danceMood(String[] moods){
+    assert moods != null;
 
+    //if types has length 0, return null
+    if (moods.length == 0){
+        return null;
+    }
+
+    //make a random variable and use nextInt to randomly choose a space
+    Random rand = new Random();
+    int randInd = rand.nextInt(moods.length);
+
+    return moods[randInd];
  }
 
 
@@ -55,11 +87,43 @@ public class DanceBuilder {
  */
 
  /*
- * @return type of dance as String from list of dances
+ * @return Sequence of dance choreography
  */
  private static Sequence<Integer> choreography(){
+    Sequence<Integer> randomSequence = new Sequence1L<>();
 
+        Random random = new Random();
+
+        final int total = 10;
+        final int danceMove = 5;
+
+        //iterate 10 times to add 10 numbers
+        for (int i = 0; i < total; i++) {
+            //generate a random integer between 1 and 5 (inclusive)
+            int randomNumber = random.nextInt(danceMove) + 1;
+
+            //add the random number to the end of the sequence
+            randomSequence.add(randomSequence.length(), randomNumber);
+        }
+
+        return randomSequence;
  }
 
+/*
+ * main method
+ */
+public static void main(String[] args){
+
+    String[] types = {"salsa", "ballet", "irish", "tap", "jazz"};
+    //I'll probably need to add a constructor
+    generateType(types);
+    this.numBars();
+    String[] moods = {"happy", "sad", "intense", "relaxed"};
+    danceMood(moods);
+
+    //some dance constructor
+    choreography();
+
+}
 
 }
