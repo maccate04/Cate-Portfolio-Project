ebbc783a31b0fd052c1d615421c78ff009df282d
maccate04 <catemaclaughlin@gmail.com>
2025-10-09 00:18:24 -0400

    Methods

--- src/DanceBuilder.java
+++ src/DanceBuilder.java
@@ -1,3 +1,65 @@
+import java.lang.Math;
+
+/*
+ *
+ * DanceBuilder class
+ *
+ * {@code DanceBuilder} using Random
+ *
+ * @convention <pre>
+ * NOT SURE YET
+ * </pre>
+ *
+ * @correspondance <pre>
+ * NOT SURE YET
+ * </pre>
+ *
+ * @author Cate MacLaughlin
+ *
+ */
+
 public class DanceBuilder {
+
+/*
+ * Kernel methods ----------------------------------------------------------------
+ */
+
+/*
+ * @return type of dance as String from list of dances
+ */
+ private static String generateType(){
+
+ }
+
+
+/*
+ * @ensures number returned is even
+ *
+ * @return number of bars as int (this is an even number)
+ */
+ private final int numBars(){
+
+ }
+
+
+ /*
+ * @return type of dance as String from list of dances
+ */
+ private static String danceMood(){
+
+ }
+
+
+/*
+ * Secondary methods ----------------------------------------------------------------
+ */
+
+ /*
+ * @return type of dance as String from list of dances
+ */
+ private static Sequence<Integer> choreography(){
+
+ }
+
 
 }
